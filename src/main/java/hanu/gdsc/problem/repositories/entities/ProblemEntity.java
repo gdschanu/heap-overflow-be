6158910e0f3b05f7c69130093131b6a4b7f6bb4e
Dat Thanh <datthanhh66@gmail.com>
2022-02-27 23:39:13 +0700

    problem entities và problem repo

--- src/main/java/hanu/gdsc/problem/repositories/entities/ProblemEntity.java
+++ src/main/java/hanu/gdsc/problem/repositories/entities/ProblemEntity.java
@@ -4,20 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "problem")
@@ -25,6 +16,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 public class ProblemEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
--- src/main/java/hanu/gdsc/problem/repositories/entities/TimeLimitEntity.java
+++ src/main/java/hanu/gdsc/problem/repositories/entities/TimeLimitEntity.java
@@ -2,21 +2,11 @@ package hanu.gdsc.problem.repositories.entities;
 
 import java.util.UUID;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
-import javax.persistence.Version;
+import javax.persistence.*;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name="timeLimit")
@@ -24,6 +14,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
+@Builder
 public class TimeLimitEntity {
     @Id
     @GeneratedValue(generator = "uuid2")
