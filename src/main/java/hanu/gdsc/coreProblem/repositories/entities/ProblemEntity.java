b26669edcaa51487ec42790d143ffb64ef1d8b1f
Dat Thanh <datthanhh66@gmail.com>
2022-03-04 00:48:48 +0700

    api getById Core Problem

--- src/main/java/hanu/gdsc/coreProblem/repositories/entities/ProblemEntity.java
+++ src/main/java/hanu/gdsc/coreProblem/repositories/entities/ProblemEntity.java
@@ -17,6 +17,8 @@ import java.util.UUID;
 @Builder
 public class ProblemEntity {
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
     private String name;
