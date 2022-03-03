1a37f65c0fa0b265553c537a4f2c02e9b42b668d
Dat Thanh <datthanhh66@gmail.com>
2022-03-04 00:29:13 +0700

    api getById Core Problem

--- src/main/java/hanu/gdsc/coreProblem/controllers/GetByIdProblemApi.java
+++ src/main/java/hanu/gdsc/coreProblem/controllers/GetByIdProblemApi.java
@@ -1,7 +1,5 @@
 package hanu.gdsc.coreProblem.controllers;
 
-import java.util.UUID;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
