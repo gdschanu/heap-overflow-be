37f760816b925fce55458d3fd5ffdb0b09893e8b
Dat Thanh <datthanhh66@gmail.com>
2022-03-04 15:43:44 +0700

    sửa lại map các contest entity

--- src/main/java/hanu/gdsc/contest/repositories/entities/ContestProblemEntity.java
+++ src/main/java/hanu/gdsc/contest/repositories/entities/ContestProblemEntity.java
@@ -1,29 +1,30 @@
 package hanu.gdsc.contest.repositories.entities;
 
+import java.util.UUID;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
 import java.util.List;
-import java.util.UUID;
+
 
 @Entity
-@Table(name = "contest_contest")
+@Table(name = "contest_problem")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ContestEntity {
+public class ContestProblemEntity {
     @Id
     @Column(columnDefinition = "BINARY(16)")
     private UUID id;
     private long version;
-    private String name;
-    private String description;
-    private String startAt;
-    private String endAt;
-    private UUID createdBy;
-    @OneToMany(mappedBy = "contest")
-    private List<ContestProblemEntity> problems;
+    private int ordinal;
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID coreProblemId;
+    private int score;
+    @ManyToOne
+    @JoinColumn(name = "contest_uuid")
+    private ContestEntity contest;
 }
