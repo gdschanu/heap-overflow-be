b1c8eb11a1cb7ad9d3c7915a62cdd2daeb16f2e8
quanvda <quandang2102@gmail.com>
2022-03-30 11:35:02 +0700

    submission event

---
+++ src/main/java/hanu/gdsc/coreProblem/repositories/SubmissionEventRepository.java
@@ -0,0 +1,7 @@
+package hanu.gdsc.coreProblem.repositories;
+
+import hanu.gdsc.coreProblem.domains.SubmissionEvent;
+
+public interface SubmissionEventRepository {
+    public void save(SubmissionEvent event);
+}
