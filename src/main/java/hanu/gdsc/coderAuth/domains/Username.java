049a4543c6067e79b88533a9c3b4c7c3b0261ffa
Damlinh0102 <dtlinh010202@gmail.com>
2022-03-31 18:22:50 +0700

    chính tả

--- src/main/java/hanu/gdsc/coderAuth/domains/Username.java
+++ src/main/java/hanu/gdsc/coderAuth/domains/Username.java
@@ -8,10 +8,10 @@ public class Username {
     public Username(String value) {
         this.value = value;
         if(value.length() < 8) {
-            throw new BusinessLogicError("username cần tối thiểu 8 kí tự", "INVALID_USERNAME");
+            throw new BusinessLogicError("Username cần tối thiểu 8 kí tự", "INVALID_USERNAME");
         }
         if (Email.isValidEmail(value)) {
-            throw new BusinessLogicError("username không được phép có định dạng email", "INVALID_USERNAME");
+            throw new BusinessLogicError("Username không được phép có định dạng email", "INVALID_USERNAME");
         }
     }
     @Override
