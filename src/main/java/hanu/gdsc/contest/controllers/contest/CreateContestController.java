005f12137f5803084785d46a0e1a78bafccb416b
quanvda <quandang2102@gmail.com>
2022-04-17 00:13:58 +0700

    update contest problem

--- src/main/java/hanu/gdsc/contest/controllers/contest/CreateContestController.java
+++ src/main/java/hanu/gdsc/contest/controllers/contest/CreateContestController.java
@@ -12,8 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class CreateContestController {
     @Autowired
     private CreateContestService createContestService;
