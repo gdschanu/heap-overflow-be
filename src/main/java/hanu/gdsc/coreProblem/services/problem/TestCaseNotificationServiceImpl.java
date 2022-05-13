c5956c3a065e9d0bf0edddc4749c47babb01a13a
quanvda <quandang2102@gmail.com>
2022-05-13 09:49:18 +0700

    socket

--- src/main/java/hanu/gdsc/coreProblem/services/problem/TestCaseNotificationServiceImpl.java
+++ src/main/java/hanu/gdsc/coreProblem/services/problem/TestCaseNotificationServiceImpl.java
@@ -1,5 +1,6 @@
 package hanu.gdsc.coreProblem.services.problem;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hanu.gdsc.coreProblem.config.TestCaseNotificationSocketConfig;
 import org.springframework.stereotype.Service;
@@ -8,27 +9,42 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Queue;
 
 @Service
 public class TestCaseNotificationServiceImpl implements TestCaseNotificationService {
-    private ServerSocket serverSocket;
-    private Socket clientSocket;
-    private PrintWriter out;
-    private ObjectMapper objectMapper;
+    private Queue<Input> runningTestCasesQueue;
 
     public TestCaseNotificationServiceImpl() throws IOException {
-        serverSocket = new ServerSocket(TestCaseNotificationSocketConfig.PORT);
-        clientSocket = serverSocket.accept();
-        out = new PrintWriter(clientSocket.getOutputStream(), true);
-        objectMapper = new ObjectMapper();
-    }
+        Thread socketHandlerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    ServerSocket serverSocket = new ServerSocket(TestCaseNotificationSocketConfig.PORT);
+                    Socket clientSocket = serverSocket.accept();
+                    PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    while (true) {
+                        Input runningTC = runningTestCasesQueue.poll();
+                        if (runningTC != null) {
+                            try {
+                                String js = objectMapper.writeValueAsString(runningTC);
+                                out.write(js);
+                            } catch (JsonProcessingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    }
+                } catch (Exception e) {
 
-    private void writeToSocket(String message) {
-        out.println(message);
+                }
+            }
+        });
+        socketHandlerThread.start();
     }
 
     @Override
     public void notifyRunningTestCase(Input input) throws Exception {
-        writeToSocket(objectMapper.writeValueAsString(input));
+        runningTestCasesQueue.add(input);
     }
 }
