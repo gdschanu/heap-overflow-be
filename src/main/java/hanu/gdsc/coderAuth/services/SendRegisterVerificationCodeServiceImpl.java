d7b9e514a300b591b534c2fe2d231cd7ff3b55ba
Damlinh0102 <dtlinh010202@gmail.com>
2022-05-15 22:40:05 +0700

    send mail

--- src/main/java/hanu/gdsc/coderAuth/services/SendRegisterVerificationCodeServiceImpl.java
+++ src/main/java/hanu/gdsc/coderAuth/services/SendRegisterVerificationCodeServiceImpl.java
@@ -1,12 +1,6 @@
 package hanu.gdsc.coderAuth.services;
 
-import javax.mail.MessagingException;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import hanu.gdsc.coderAuth.domains.Email;
@@ -14,7 +8,6 @@ import hanu.gdsc.coderAuth.domains.RegisterVerificationCode;
 import hanu.gdsc.coderAuth.domains.User;
 import hanu.gdsc.coderAuth.repositories.RegisterVerificationCodeRepository;
 import hanu.gdsc.coderAuth.repositories.UserRepository;
-import hanu.gdsc.share.domains.DateTime;
 import hanu.gdsc.share.domains.Id;
 import hanu.gdsc.share.error.BusinessLogicError;
 
@@ -28,39 +21,19 @@ public class SendRegisterVerificationCodeServiceImpl implements SendRegisterVeri
     private RegisterVerificationCodeRepository registerVerificationCodeRepository;
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private SendMailService sendMailService;
 
     @Override
     public void sendRegisterVerificationCodeService(Id coderId) {
         User user = userRepository.getByCoderId(coderId);
+        if(user == null) {
+            throw new BusinessLogicError("Your token is expired", "EXPIRED_TOKEN");
+        }
         String name = user.getUsername().toString();
         Email toAddress = user.getEmail();
-        Email fromAddress = new Email("dtlinh010202@gmail.com");
-        String subject = "Verify your email to register";
-        String code = RegisterVerificationCode.generateRandom();
-        DateTime expireAt = DateTime.now().plusMinutes(100);
-        RegisterVerificationCode registerVerificationCode = new RegisterVerificationCode(coderId, code, expireAt);
+        RegisterVerificationCode registerVerificationCode = RegisterVerificationCode.createRegisterVerificationCode(coderId);
         registerVerificationCodeRepository.save(registerVerificationCode);
 
-        String content = "Dear " + name + ",\n"
-        +"Use this code below to verify your account \n"
-        + code +"\n"
-        + "Thanks,\n"
-        +"Hanu gcsd";
-
-        MimeMessage message = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(message);
-        
-        try {
-            helper.setFrom(new InternetAddress(fromAddress.toString()));
-            helper.setTo(new InternetAddress(toAddress.toString()));
-            helper.setSubject(subject);
-            helper.setText(content);
-
-        } catch (MessagingException exception) {
-            throw new BusinessLogicError("Error in sending email", "EMAIL_SENDING_ERROR");
-        }
-        javaMailSender.send(message);
+        sendMailService.sendMail(toAddress, name, registerVerificationCode.getCode());      
     }
-
 }
