6ed82d0d122ae3161fe16fda90b59c710e8e69ea
Damlinh0102 <dtlinh010202@gmail.com>
2022-08-05 16:28:06 +0700

    coderRanking

--- src/main/java/hanu/gdsc/coder/domains/Url.java
+++ src/main/java/hanu/gdsc/coder/domains/Url.java
@@ -1,4 +1,9 @@
 package hanu.gdsc.coder.domains;
 
 public class Url {
+    private String value;
+
+    public Url(String value) {
+        this.value = value;
+    }
 }
--- src/main/java/hanu/gdsc/coder/repositories/CoderRepositoryImpl.java
+++ src/main/java/hanu/gdsc/coder/repositories/CoderRepositoryImpl.java
@@ -5,6 +5,9 @@ import hanu.gdsc.coder.domains.Coder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Repository
 public class CoderRepositoryImpl implements CoderRepository {
     @Autowired
@@ -13,5 +16,11 @@ public class CoderRepositoryImpl implements CoderRepository {
     @Override
     public void create(Coder coder) {
         coderJpaRepository.save(CoderEntity.fromDomains(coder));
+    }
+
+    @Override
+    public List<Coder> getAll() {
+        List<CoderEntity> coderEntities = coderJpaRepository.findAll();
+        return coderEntities.stream().map(x -> x.toDomain()).collect(Collectors.toList());
     }
 }
