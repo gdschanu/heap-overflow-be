d66766ffcf48a45aeed324cb75176c4226f34c48
quanvda <quandang2102@gmail.com>
2022-08-27 23:43:15 +0700

    socket

--- src/main/java/hanu/gdsc/share/config/WebConfig.java
+++ src/main/java/hanu/gdsc/share/config/WebConfig.java
@@ -11,12 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("Origin", "Accept", "Content-Type", "Authorization")
-                .allowCredentials(true)
-                .maxAge(3600);
+        registry.addMapping("/**");
     }
 
     public void addResourceHandlers(ResourceHandlerRegistry registry)
