4b6ce5ac88791cf6778c7d7193df8a790d1a8e57
quanvda <quandang2102@gmail.com>
2022-08-28 18:48:40 +0700

    u

--- src/main/java/hanu/gdsc/practiceProblem_problem/config/RunningSubmissionConfig.java
+++ src/main/java/hanu/gdsc/practiceProblem_problem/config/RunningSubmissionConfig.java
@@ -1,18 +1,18 @@
 package hanu.gdsc.practiceProblem_problem.config;
 
 public class RunningSubmissionConfig {
-    public static final int MAX_THREAD = 2;
-    public static final int RATE_MILLIS = 5000;
-    public static final int CLAIM_SUBMISSION_LOCK_SECOND = 60 * 5;
+    public static  int MAX_THREAD = 2;
+    public static  int RATE_MILLIS = 5000;
+    public static  int CLAIM_SUBMISSION_LOCK_SECOND = 60 * 5;
 
-    public static final int SLEEP_WAITING_FOR_SUBMISSION_MILLIS = 100;
+    public static  int SLEEP_WAITING_FOR_SUBMISSION_MILLIS = 100;
 
 
-    public static final int PORT = 5000;
-    public static final long GET_RUNNING_SUBMISSION_INFO_RATE_MILLIS = 5000;
-    public static final long UPDATE_RUNNING_SUBMISSION_RATE_MILLIS = 3000;
+    public static  int PORT = 5000;
+    public static  long GET_RUNNING_SUBMISSION_INFO_RATE_MILLIS = 5000;
+    public static  long UPDATE_RUNNING_SUBMISSION_RATE_MILLIS = 3000;
 
-    public static final long DELETE_JUDGER_SUBMISSION_RATE_MILLIS = 5000;
+    public static  long DELETE_JUDGER_SUBMISSION_RATE_MILLIS = 5000;
 
-    public static final String IP = "103.183.113.65";
+    public static  String IP = "103.183.113.65";
 }
