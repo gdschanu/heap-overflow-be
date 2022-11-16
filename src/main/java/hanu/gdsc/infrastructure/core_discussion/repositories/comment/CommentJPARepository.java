a35d7fdf4a10da4e381d950bfc5cb3ba876460fb
quanvda <quandang2102@gmail.com>
2022-11-16 23:02:52 +0700

    delete problem

--- src/main/java/hanu/gdsc/infrastructure/core_discussion/repositories/comment/CommentJPARepository.java
+++ src/main/java/hanu/gdsc/infrastructure/core_discussion/repositories/comment/CommentJPARepository.java
@@ -1,6 +1,7 @@
 package hanu.gdsc.infrastructure.core_discussion.repositories.comment;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Set;
@@ -9,5 +10,6 @@ import java.util.Set;
 public interface CommentJPARepository extends JpaRepository<CommentEntity, String> {
     Set<CommentEntity> findAllByPostIdAndServiceToCreate(String postId, String serviceToCreate);
     @Query("DELETE FROM CommentEntity c WHERE c.postId in (:postIds)")
+    @Modifying
     void deleteAllByPostIds(Iterable<String> postIds);
 }
